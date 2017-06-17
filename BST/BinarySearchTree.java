ef840f88413fa9acd38cc90aad8a6c20b8f889a1
RichardJCai <caioftherichard@gmail.com>
2017-06-17 00:28:32 -0400

    Added iterative floor and recursive ceil, todo: add DeleteMin, DeleteMax and Delete(Key)

--- BST/BinarySearchTree.java
+++ BST/BinarySearchTree.java
@@ -7,11 +7,13 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
   public class Node{
     private Key key;
     private Value val;
+    public int size; //# of nodes in the subtree
     public Node left, right;
 
-    public Node(Key key, Value val){
+    public Node(Key key, Value val,int size){
       this.key = key;
       this.val = val;
+      this.size = size;
     }
   }
 
@@ -20,11 +22,21 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
   }
 
   public Node put(Node node, Key key, Value val){
-    if (node == null) return new Node(key,val);
+    if (node == null) return new Node(key,val,1);
     int cmp = key.compareTo(node.key);
     if (cmp < 0) node.left = put(node.left,key,val);
     else if (cmp > 0) node.right = put(node.right,key,val);
+    node.size = 1 + size(node.right) + size(node.left);
     return node;
+  }
+
+  public int size(){
+    return size(root);
+  }
+
+  private int size(Node node){
+    if (node == null) return 0;
+    else return node.size;
   }
 
   public Value get(Key key){
@@ -37,6 +49,45 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
     }
     return null;
   }
+
+  public Key floor(Key key){
+    //Non recursive implmentation
+    Node currentNode = root;
+
+    while (true){
+      if (currentNode == null) return null;
+      int cmp = key.compareTo(currentNode.key);
+
+      if (cmp == 0) return currentNode.key;
+      else if (cmp < 0) currentNode = currentNode.left;
+      else if (cmp > 0){
+        while (true){
+          if (currentNode.right == null) return currentNode.key;
+          cmp = key.compareTo(currentNode.right.key);
+          if (cmp < 0) return currentNode.key;
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  public Key ceil(Key key){
+    //Recursive implmentation of ceil
+    return ceil(key,root).key;
+  }
+
+  private Node ceil(Key key, Node node){
+    if (node == null) return null;
+    int cmp = key.compareTo(node.key);
+    if (cmp == 0) return node;
+    else if (cmp < 0){
+      Node retNode = ceil(key,node.left);
+      if (retNode != null) return retNode;
+      else return node;
+    }
+    return ceil(key,node.right);
+  }
+
 
   public void delete(Key key){
     Node currentNode = root;
--- BST/testBST.java
+++ BST/testBST.java
@@ -1,14 +1,18 @@
 public class testBST extends BinarySearchTree{
   public static void main(String args[]){
     BinarySearchTree bst = new BinarySearchTree();
+    bst.put(10,10);
+    bst.put(6,6);
+    bst.put(11,11);
+    bst.put(7,7);
+    bst.put(12,12);
+    bst.put(3,3);
+    bst.put(5,5);
+    bst.put(24,24);
     bst.put(1,1);
-    bst.put(2,2);
-    bst.put(7,2);
-    bst.put(0,2);
-    bst.put(-100,2);
-    bst.put(-1,2);
-    bst.put(3,2);
-    bst.delete(1);
+    System.out.println(bst.floor(8));
+    System.out.println(bst.ceil(15));
+    System.out.println(bst.size());
     Iterable iterator = bst.iterator();
     // while (iterator.hasNext()){
     //   System.out.print(iterator.next());
